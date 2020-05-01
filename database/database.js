9b7bdd0140cad6cee3964b09d791ab1b1ce599e9
Eli Caraballo <eli.caraballojr@gmail.com>
2020-05-01 18:46:28 -0500

    Set up Schema, Need to populate

--- database/database.js
+++ database/database.js
@@ -1,1 +1,21 @@
 const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/imagesData', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', () => {
+  console.log('Connected to Database');
+  const imageDataSchema = new mongoose.Schema({
+    gameTitle: String,
+    newPrice: Number,
+    preOwnedPrice: Number,
+    digitalPrice: Number,
+    mainImage: String,
+    images: [String],
+  });
+
+  const ImageData = mongoose.model('ImageData', imageDataSchema);
+});
--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-// const axios = require('axios');
-// const db = require('../database/.database.js')
+const db = require('../database/database.js');
 
 const app = express();
 const PORT = 3003;
 
 app.use(express.static(`${__dirname}/../public`));
 app.use(express.json());
+
+app.get();
 
 app.listen(PORT, () => {
   console.log(`Listening at port:${PORT}`);
