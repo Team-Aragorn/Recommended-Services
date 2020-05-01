8d5eaad4171a726b9c384f113421425931b914ba
Eli Caraballo <eli.caraballojr@gmail.com>
2020-04-30 22:46:32 -0500

    Fixed Linting Errors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const axios = require('axios');
+// const axios = require('axios');
 // const db = require('../database/.database.js')
 
 const app = express();
 const PORT = 3003;
 
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(`${__dirname}/../public`));
 app.use(express.json());
 
 app.listen(PORT, () => {
