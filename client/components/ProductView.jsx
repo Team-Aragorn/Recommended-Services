d74f8142ae03c0f8ae27d53a430ba97f88a26d54
Eli Caraballo <eli.caraballojr@gmail.com>
2020-05-05 12:02:54 -0500

    Added main Image and switching Image functionality.
    --fixed eslint errors

--- client/components/ProductView.jsx
+++ client/components/ProductView.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const ProductView = (props) => {
+  ProductView.propTypes = {
+    game: PropTypes.shape({
+      images: PropTypes.arrayOf(PropTypes.string),
+    }).isRequired,
+  };
+
+  const { game } = props;
+
+  const [mainImage, setMainImage] = useState();
+
   return (
     <div>
-      {props.game.images.map((image) => {
-        return (
-          <img key={image} src={image} alt="product images" />
-        );
-      })}
+      <div className="mainImage">
+        <img src={mainImage || game.images[0]} alt="main" />
+      </div>
+      {game.images.map((image) => (
+        <input type="image" className="imageBar" key={image} src={image} alt="product images" onClick={(event) => { setMainImage(event.target.src); }} />
+      ))}
     </div>
   );
 };
