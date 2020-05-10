383618957c4b483efd10e1efe35ae0450f45c0a7
Eli Caraballo <eli.caraballojr@gmail.com>
2020-05-10 13:19:20 -0500

    Competed styling

--- client/components/GameInfo.jsx
+++ client/components/GameInfo.jsx
@@ -1,30 +1,39 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React, { useState } from 'react';
 
 const GameInfo = (props) => {
   const { games } = props;
 
   return (
-    <div className="allgames">
-      {games.map((game) => (
-        // eslint-disable-next-line no-underscore-dangle
-        <div key={game._id} className="game">
-          <img src={game.mainImage} alt="game cover" />
-          <h3>{game.gameTitle}</h3>
-          <h4>{game.console}</h4>
-          <span className="row">
-            <h5 className="rowlable">New</h5>
-            <h4 className="rowprice">{game.newPrice}</h4>
-          </span>
-          <span className="row">
-            <h5 className="rowlable">Pre-Owned</h5>
-            <h4 className="rowprice">{game.preOwnedPrice}</h4>
-          </span>
-          <span className="row">
-            <h5 className="rowlable">Digital</h5>
-            <h4 className="rowprice">{game.digitalPrice}</h4>
-          </span>
-        </div>
-      ))}
+    <div className="spacer">
+      <div />
+      <div className="allgames">
+        {games.map((game) => (
+          // eslint-disable-next-line no-underscore-dangle
+          <div value={game._id} key={game._id} className="onegame">
+            <img className="image" src={game.mainImage} alt="game cover" />
+            <h3 className="gametitle">{game.gameTitle}</h3>
+            <h4 className="console">{game.console}</h4>
+            <div className="line" />
+            <div className="row">
+              <h5 className="rowlable">New</h5>
+              <h4 className="rowprice">${game.newPrice}</h4>
+            </div>
+            <div className="line" />
+            <div className="row">
+              <h5 className="rowlable">Pre-Owned</h5>
+              <h4 className="rowprice">${game.preOwnedPrice}</h4>
+            </div>
+            <div className="line" />
+            <div className="row">
+              <h5 className="rowlable">Digital</h5>
+              <h4 className="rowprice">${game.digitalPrice}</h4>
+            </div>
+            <div className="line" />
+          </div>
+        ))}
+      </div>
+      <div />
     </div>
   );
 };
